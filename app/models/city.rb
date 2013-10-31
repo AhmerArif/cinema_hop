317f2ccde8f73bbe4793a2183b24eda05022e329
Ahmer Arif <ahmer.arif@gmail.com>
2013-10-31 12:51:17 +0500

    Made return value array of unique cinemas

--- app/models/city.rb
+++ app/models/city.rb
@@ -11,7 +11,7 @@ class City < ActiveRecord::Base
 	end
 
 	def current_cinemas(movie)
-		movie.cinemas.includes(:showtimes).where('showtimes.showing_at >= ? AND cinemas.id in (?)', Time.now-30.minutes, default_city? ? Cinema.all : cinemas).order('cinemas.name ASC').references(:showtimes)
+		movie.cinemas.includes(:showtimes).where('showtimes.showing_at >= ? AND cinemas.id in (?)', Time.now-30.minutes, default_city? ? Cinema.all : cinemas).order('cinemas.name ASC').references(:showtimes).uniq!
 	end
 
 	def default_city?
